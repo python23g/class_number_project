3a8dcc0391bdcba09594e951dfd333e30618aa5a
AsadbekKazakovDev <asadbekkazakov5300@gmail.com>
2023-07-29 10:36:42 +0500

    solve main number.py

--- number.py
+++ number.py
@@ -9,7 +9,7 @@ class Number:
 
         returns: int
         """
-        pass
+        return self.value
 
     def is_odd(self):
         """
@@ -18,7 +18,7 @@ class Number:
         returns: bool
 
         """
-        pass
+        return self.value%2==1
 
     def is_even(self):
         """
@@ -26,7 +26,7 @@ class Number:
 
         returns: bool
         """
-        pass
+        return self.value%2==0
 
     def is_prime(self):
         """
@@ -34,7 +34,12 @@ class Number:
 
         returns: bool
         """
-        pass
+        if self.value<2:
+            return False
+        for i in range(2,self.value):
+            if self.value%i==0:
+                return False
+            return True
 
     def get_divisors(self):
         """
@@ -42,7 +47,12 @@ class Number:
 
         returns: list
         """
-        pass
+        ans=[]
+        for i in range(1,self.value+1):
+            if self.value%i==0:
+                ans.append(i)
+        return ans
+                
 
     def get_length(self):
         """
@@ -50,7 +60,8 @@ class Number:
 
         returns: int
         """
-        pass
+        m = len(str(self.value))
+        return m
 
     def get_sum(self):
         """
@@ -58,7 +69,12 @@ class Number:
 
         returns: int
         """
-        pass
+        ds = str(self.value)
+        answ1 = []
+        
+        for i in range(len(ds)):
+            answ1.append(int(ds[i]))
+        return sum(answ1)
 
     def get_reverse(self):
         """
@@ -66,7 +82,8 @@ class Number:
 
         returns: int
         """
-        pass
+        b = str(self.value)[::-1]
+        return int(b)
 
     def is_palindrome(self):
         """
@@ -74,7 +91,8 @@ class Number:
 
         returns: bool
         """
-        pass
+        c = str(self.value)[::-1]
+        return c==str(self.value)
 
     def get_digits(self):
         """
@@ -82,7 +100,11 @@ class Number:
 
         returns: list
         """
-        pass
+        d = str(self.value)
+        answ = []
+        for i in range(len(d)):
+            answ.append(int(d[i]))
+        return answ
 
     def get_max(self):
         """
@@ -90,7 +112,13 @@ class Number:
 
         returns: int
         """
-        pass
+        ds = str(self.value)
+        answ1 = []
+        
+        for i in range(len(ds)):
+            answ1.append(int(ds[i]))
+        return max(answ1)
+        
 
     def get_min(self):
         """
@@ -98,7 +126,12 @@ class Number:
 
         returns: int
         """
-        pass
+        ds = str(self.value)
+        answ1 = []
+        
+        for i in range(len(ds)):
+            answ1.append(int(ds[i]))
+        return min(answ1)
 
     def get_average(self):
         """
@@ -106,7 +139,12 @@ class Number:
 
         returns: float
         """
-        pass
+        ds = str(self.value)
+        answ1 = []
+        
+        for i in range(len(ds)):
+            answ1.append(int(ds[i]))
+        return sum(answ1)/len(ds)
 
     def get_median(self):
         """
@@ -114,7 +152,19 @@ class Number:
 
         returns: float
         """
-        pass
+        digits = [int(digit) for digit in str(self.value)]  # Extract digits
+        sorted_digits = sorted(digits)  # Sort the digits
+        length = len(sorted_digits)
+    
+    # Find the median
+        if length % 2 == 0:  # If the length is even
+            mid = length // 2  # Find the middle index
+            median = (sorted_digits[mid - 1] + sorted_digits[mid]) / 2
+        else:  # If the length is odd
+            mid = length // 2  # Find the middle index
+            median = sorted_digits[mid]
+    
+        return median
 
     def get_range(self):
         """
@@ -122,7 +172,14 @@ class Number:
 
         returns: list
         """
-        pass
+        if self.value<10:
+            return [self.value]
+        ds = str(self.value)
+        answ1 = []
+        
+        for i in range(len(ds)):
+            answ1.append(int(ds[i]))
+        return [min(answ1),max(answ1)]
 
     def get_frequency(self):
         """
@@ -130,8 +187,38 @@ class Number:
 
         returns: dict
         """
-        pass
+        # Convert number to a list of digits
+        digits = [int(digit) for digit in str(self.value)]
     
+    # Initialize an empty dictionary for frequencies
+        frequency_dict = {}
+    
+    # Count the frequency of each digit using a loop
+        for digit in digits:
+            if digit in frequency_dict:
+                frequency_dict[digit] += 1
+            else:
+                frequency_dict[digit] = 1
+    
+        return frequency_dict
 
 # Create a new instance of Number
-number = Number(3)
+number = Number(363363)
+print(number.get_number())
+print(number.is_odd())
+print(number.is_even())
+print(number.is_prime())
+print(number.get_divisors())
+#print(number.get_digits())
+print(number.get_length())
+print(number.get_sum())
+print(number.get_reverse())
+print(number.is_palindrome())
+print(number.get_digits())
+print(number.get_max())
+print(number.get_min())
+print(number.get_average())
+print(number.get_median())
+print(number.get_range())
+print(number.get_frequency())
+
